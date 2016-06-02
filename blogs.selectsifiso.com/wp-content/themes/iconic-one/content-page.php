a84dea02ee7bece42a9a5523f8a2edf806b3dae2
@mafiswana <sinndlovu@clientele.co.za>
2016-06-02 17:42:50 +0200

    theme editor

--- blogs.selectsifiso.com/wp-content/themes/iconic-one/content-page.php
+++ blogs.selectsifiso.com/wp-content/themes/iconic-one/content-page.php
@@ -16,8 +16,5 @@
 		<div class="entry-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'themonic' ), 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-		<footer class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'themonic' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- .entry-meta -->
+		</div><!-- .entry-content -->		
 	</article><!-- #post -->
